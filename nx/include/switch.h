8858f29c6dc762c12e4743617c253f523085db56
plutooo <plutooo@users.noreply.github.com>
2018-02-26 22:55:05 +0100

    Stubbed 3d_ctx

--- nx/include/switch.h
+++ nx/include/switch.h
@@ -76,6 +76,7 @@ extern "C" {
 #include "switch/nvidia/info.h"
 #include "switch/nvidia/gpu/gpfifo.h"
 #include "switch/nvidia/gpu/zcull_ctx.h"
+#include "switch/nvidia/gpu/3d_ctx.h"
 #include "switch/nvidia/gpu/gpu.h"
 
 #include "switch/runtime/env.h"
