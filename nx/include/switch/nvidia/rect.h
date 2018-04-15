014d7df5735df107f5053d167aa00130c03e5f41
plutooo <plutooo@users.noreply.github.com>
2018-04-15 19:32:06 +0200

    Added VnViewport

---
+++ nx/include/switch/nvidia/rect.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define MAKE_RECT(type) \
+    typedef struct { \
+        type x; \
+        type y; \
+        type w; \
+        type h; \
+    } type##_rect;
+
+MAKE_RECT(u16);
+MAKE_RECT(u32);
+MAKE_RECT(float);
+MAKE_RECT(double);
