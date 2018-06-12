c7173efa37f95aff1d4691e5ce3fa6bfd12f5f72
Jules Blok <jules.blok@gmail.com>
2018-06-12 13:42:09 +0200

    3d_viewport: Fix float to int conversion.

--- nx/source/nvidia/cmds/3d_viewport.c
+++ nx/source/nvidia/cmds/3d_viewport.c
@@ -7,12 +7,12 @@ void vnSetViewport(Vn* vn, size_t index, VnViewportConfig* c) {
         NvIncr(
             0,
             NvReg3D_ViewportScaleX(index),
-            c->scale[0], // ScaleX
-            c->scale[1], // ScaleY
-            c->scale[2], // ScaleZ
-            c->translate[0], // TranslateX
-            c->translate[1], // TranslateY
-            c->translate[2], // TranslateZ
+            f2i(c->scale[0]), // ScaleX
+            f2i(c->scale[1]), // ScaleY
+            f2i(c->scale[2]), // ScaleZ
+            f2i(c->translate[0]), // TranslateX
+            f2i(c->translate[1]), // TranslateY
+            f2i(c->translate[2]), // TranslateZ
             0, // Swizzles
             0  // SubpixelPrecisionBias
         ),
